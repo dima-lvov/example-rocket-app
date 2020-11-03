703d274537440b8b11acbf1fbde9ad98086a9742
Paris Liakos <paris.liakos@gmail.com>
2020-11-03 23:06:55 +0100

    Part #9 - Auth cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
 
-use rocket::http::Status;
-use rocket::request::{Request, FromRequest, Outcome};
+mod auth;
+
+use auth::BasicAuth;
 use rocket::response::status;
 use rocket_contrib::json::JsonValue;
 
@@ -30,59 +31,6 @@ fn delete_rustacean(_id: i32, _auth: BasicAuth) -> status::NoContent {
 #[catch(404)]
 fn not_found() -> JsonValue {
     json!("Not found!")
-}
-
-pub struct BasicAuth {
-    pub username: String,
-    pub password: String,
-}
-impl BasicAuth {
-    fn from_authorization_header(header: &str) -> Option<BasicAuth> {
-        let split = header.split_whitespace().collect::<Vec<_>>();
-        if split.len() != 2 {
-            return None;
-        }
-
-        if split[0] != "Basic" {
-            return None;
-        }
-
-        Self::from_base64_encoded(split[1])
-    }
-
-    fn from_base64_encoded(base64_string: &str) -> Option<BasicAuth> {
-        let decoded = base64::decode(base64_string).ok()?;
-        let decoded_str = String::from_utf8(decoded).ok()?;
-        let split = decoded_str.split(":").collect::<Vec<_>>();
-
-        // If exactly username & password pair are present
-        if split.len() != 2 {
-            return None;
-        }
-
-        let (username, password) = (split[0].to_string(), split[1].to_string());
-
-        Some(BasicAuth {
-            username,
-            password
-        })
-    }
-}
-
-#[rocket::async_trait]
-impl<'a, 'r> FromRequest<'a, 'r> for BasicAuth {
-    type Error = ();
-
-    async fn from_request(request: &'a Request<'r>) -> Outcome<Self, Self::Error> {
-        let auth_header = request.headers().get_one("Authorization");
-        if let Some(auth_header) = auth_header {
-            if let Some(auth) = Self::from_authorization_header(auth_header) {
-                return Outcome::Success(auth)
-            }
-        }
-        
-        Outcome::Failure((Status::Unauthorized, ()))
-    }
 }
 
 #[rocket::main]
