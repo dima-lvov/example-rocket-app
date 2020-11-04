4d714eeaad1aa03461c71183a853a1b1cb7debb0
Paris Liakos <paris.liakos@gmail.com>
2020-11-04 19:36:48 +0100

    Parts #11 & #12 - Dependencies and Rustacean model

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate diesel;
 
 mod auth;
+mod models;
+mod schema;
 
+use diesel::prelude::*;
+use models::*;
+use schema::*;
 use auth::BasicAuth;
 use rocket::response::status;
 use rocket_contrib::json::JsonValue;
 
+#[database("sqlite_path")]
+struct DbConn(diesel::SqliteConnection);
+
 #[get("/rustaceans")]
-fn get_rustaceans(_auth: BasicAuth) -> JsonValue {
-    json!([{ "id": 1, "name": "John Doe" }, { "id": 2, "name": "John Doe again" }])
+async fn get_rustaceans(_auth: BasicAuth, conn: DbConn) -> JsonValue {
+    conn.run(|c| {
+        let all = rustaceans::table.limit(100).load::<Rustacean>(c).expect("Error loading rustaceans from DB");
+        json!(all)
+    }).await
 }
 #[get("/rustaceans/<id>")]
 fn view_rustacean(id: i32, _auth: BasicAuth) -> JsonValue {
@@ -46,6 +58,7 @@ async fn main() {
         .register(catchers![
             not_found
         ])
+        .attach(DbConn::fairing())
         .launch()
         .await;
 }
